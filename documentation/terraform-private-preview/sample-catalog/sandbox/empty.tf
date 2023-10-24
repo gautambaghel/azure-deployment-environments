8cfa1a60e3b3285ca142f96178dd996118753ebb
Gautam <gautambaghel93@gmail.com>
2023-10-24 12:07:41 -0700

    hacky

--- documentation/terraform-private-preview/sample-catalog/sandbox/empty.tf
+++ documentation/terraform-private-preview/sample-catalog/sandbox/empty.tf
@@ -1,1 +1,50 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.75"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.28.0"
+    }
+  }
 
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+
+  skip_provider_registration = true
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  token      = var.AWS_SESSION_TOKEN
+}
+
+variable "AWS_ACCESS_KEY_ID" {}
+
+variable "AWS_SECRET_ACCESS_KEY" {}
+
+variable "AWS_SESSION_TOKEN" {}
+
+data "aws_ami" "amazon2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+
+  owners = ["amazon"] # Canonical
+}
+
+resource "aws_instance" "ec2" {
+  ami                         = data.aws_ami.amazon2.id
+  instance_type               = "t3.nano"
+  associate_public_ip_address = true
+}
